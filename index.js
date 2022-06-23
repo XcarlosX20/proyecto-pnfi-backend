c3d9073cea0694df0ac2c3cc8f285a5e87d7cd30
Carlos Sierra <carlossierra850@gmail.com>
2022-06-22 23:02:12 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ require('dotenv').config({ path: '.env' })
 const conectarDB = require('./config/db')
 conectarDB()
 const PORT = process.env.PORT || 4000
-app.use(delay(1000))
 app.use(cors())
 app.use(express.json({ extended: true }))
 // app.use('/api/auth', require('./routes/auth'))
